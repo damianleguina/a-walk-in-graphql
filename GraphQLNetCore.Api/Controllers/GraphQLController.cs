866cefdf17dccc70bf864ea8896a14b95aab1366
damianleguina <Damian.Leguina@Wellsky.com>
2020-06-22 13:04:06 -0300

    - Advances in infrastructure

---
+++ GraphQLNetCore.Api/Controllers/GraphQLController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using GraphQLNetCore.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphQLNetCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphQLController : ControllerBase
+    {
+        private readonly ISchema schema;
+        private readonly IDocumentExecuter documentExecuter;
+        public GraphQLController(ISchema schema, IDocumentExecuter documentExecuter)
+        {
+            this.schema = schema;
+            this.documentExecuter = documentExecuter;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            if (query == null)
+                throw new ArgumentNullException(nameof(query));
+
+            var inputs = query.Variables?.ToInputs();
+            var executionOption = new ExecutionOptions
+            {
+                Schema = schema,
+                Query = query.Query,
+                Inputs = inputs
+            };
+
+            var result = await documentExecuter.ExecuteAsync(executionOption);
+
+            if (result.Errors.Any())
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ GraphQLNetCore.Data/GraphQLContext.cs
@@ -0,0 +1,15 @@
+﻿using GraphQLNetCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLNetCore.Data
+{
+    public class GraphQLContext : DbContext
+    {
+        public GraphQLContext(DbContextOptions<GraphQLContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
