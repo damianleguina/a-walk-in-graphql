55c8286c58051c57d023c704f618269716778557
damianleguina <Damian.Leguina@Wellsky.com>
2020-06-22 13:04:06 -0300

    - Advances in Infraestructure

---
+++ GraphQLNetCore.Entities/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphQLNetCore.Entities
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public string AgeColor { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string FavSkill { get; set; }
+        public ICollection<Person> Friends { get; set; }
+        public ICollection<Skill> Skills { get; set; }
+    }
+}
---
+++ GraphQLNetCore.Entities/Skill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphQLNetCore.Entities
+{
+    public class Skill
+    {
+        public string Id { get; set; }
+        public string Parent { get; set; }
+        public string Name { get; set; }
+    }
+}
