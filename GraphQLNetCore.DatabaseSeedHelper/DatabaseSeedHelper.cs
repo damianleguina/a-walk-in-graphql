ccdbbd7e3c1fef427f0687a34421d55a9dd5c495
damianleguina <Damian.Leguina@Wellsky.com>
2020-06-23 14:52:05 -0300

    - Improvements in DatabaseSeedHelper.

--- GraphQLNetCore.DatabaseSeedHelper/DatabaseSeedHelper.cs
+++ GraphQLNetCore.DatabaseSeedHelper/DatabaseSeedHelper.cs
@@ -2,9 +2,7 @@
 using GraphQLNetCore.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace GraphQLNetCore.DatabaseSeedHelper
 {
@@ -19,30 +17,9 @@ namespace GraphQLNetCore.DatabaseSeedHelper
                 List<Person> people;
                 (skills, people) = JsonHelper.GetEntities();
                 context.AddRange(skills);
+                context.AddRange(people);
                 context.SaveChanges();
-                foreach (var person in people)
-                {
-                    NormalizeLists(person, context);
-                    context.Add(person);
-                    context.SaveChanges();
-                }
             }
-        }
-
-        private static void NormalizeLists(Person person, GraphQLContext context)
-        {
-            var skills = person.Skills.ToList();
-            for (int i = 0; i < skills.Count; i++)
-            {
-                skills[i] = context.Skills.FirstOrDefault(x => x.Id == skills[i].Id);
-            }
-            var friends = person.Friends.ToList();
-            for (int i = 0; i < friends.Count; i++)
-            {
-                friends[i] = context.People.FirstOrDefault(x => x.Id == friends[i].Id);
-            }
-            person.Skills = skills;
-            person.Friends = friends;
         }
     }
 }
--- GraphQLNetCore.DatabaseSeedHelper/JsonHelper.cs
+++ GraphQLNetCore.DatabaseSeedHelper/JsonHelper.cs
@@ -1,29 +1,54 @@
 ﻿using GraphQLNetCore.DatabaseSeedHelper.Models;
 using GraphQLNetCore.DatabaseSeedHelper.Utils;
 using GraphQLNetCore.Entities;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace GraphQLNetCore.DatabaseSeedHelper
 {
     internal static class JsonHelper
     {
+        #region Public Methods
+
         public static (List<Skill>, List<Person>) GetEntities()
         {
             SeedJsonModel seed = GetSeed();
             SeedJsonMapper seedJsonMapper = new SeedJsonMapper();
             List<Skill> skills = seedJsonMapper.MapSkills(seed.Skills);
             List<Person> people = seedJsonMapper.MapPeople(seed.People);
+            NormalizePeopleProperties(people, skills);
             return (skills, people);
         }
 
+        #endregion Public Methods
+
+        #region Private Methods
 
         private static SeedJsonModel GetSeed()
         {
             SeedDeserializer seedDeserializer = new SeedDeserializer();
-            var seed = seedDeserializer.GetDeserializedSeedJson();
-            return seed;
+            return seedDeserializer.GetDeserializedSeedJson();
         }
+
+        private static void NormalizePeopleProperties(List<Person> people, List<Skill> allSkills)
+        {
+            foreach (var person in people)
+            {
+                var personSkills = person.Skills.ToList();
+                for (int i = 0; i < personSkills.Count; i++)
+                {
+                    personSkills[i] = allSkills.Find(x => x.Id == personSkills[i].Id);
+                }
+                var friends = person.Friends.ToList();
+                for (int i = 0; i < friends.Count; i++)
+                {
+                    friends[i] = people.Find(x => x.Id == friends[i].Id);
+                }
+                person.Skills = personSkills;
+                person.Friends = friends;
+            }
+        }
+
+        #endregion Private Methods
     }
 }
